a455263f58a22976750ef0a150e37a09cfc26587
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-07-11 09:04:38 +0200

    Added tests support

---
+++ src/components/Home.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {shallow} from 'enzyme';
+import Home from './Home';
+
+describe('Home', () => {
+  it('should render without failure', () => {
+    const component = shallow(<Home/>);
+    
+    expect(component.length).toBe(1);
+  });
+})
