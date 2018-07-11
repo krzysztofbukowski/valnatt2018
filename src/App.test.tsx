777a118180be4ec3830e0a0e83a39cf6f1c80742
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-07-11 12:33:47 +0200

    Updated formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
   it('should render without failure', () => {
-    const component = shallow(<App name="test"/>);
-    
+    const component = shallow(<App name="test" />);
+
     expect(component.length).toBe(1);
   });
 })
