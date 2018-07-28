a98490f2acde1d371dd97c5d327233015ee5a78c
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-07-28 23:14:44 +0200

    Made use of autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const devMode = process.env.NODE_ENV !== 'production';
 
+const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 const path = require('path');
 
@@ -51,6 +52,26 @@ const config = {
               }
             }
           },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            }
+          },          
           {
             loader: 'sass-loader',
             options: {
