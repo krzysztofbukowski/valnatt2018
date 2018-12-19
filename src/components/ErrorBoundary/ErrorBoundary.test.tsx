0debf911ff1c9a4195601e77dbc54b405872a523
Krzysztof Bukowski <krzysztof.bukowski@schibsted.com>
2018-12-19 14:04:59 +0100

    Fixed error boundary

--- src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,11 +1,14 @@
+import * as React from 'react';
 import ErrorBoundary, { withErrorBoundary } from './ErrorBoundary';
 import { mount } from 'enzyme';
+import { Fragment } from 'react';
 
 describe(`${withErrorBoundary.name}`, () => {
   it(`should wrap component with ${ErrorBoundary.name}`, () => {
     const MockedComponent = () => 'test';
+    const FallbackUIComponent: Element = () => <div></div>;
 
-    const WithErrorBoundaryComponent = withErrorBoundary(MockedComponent, '')({test: 1});
+    const WithErrorBoundaryComponent = withErrorBoundary(MockedComponent, FallbackUIComponent)({test: 1});
 
     expect(mount(WithErrorBoundaryComponent).is(ErrorBoundary)).toBeTruthy();
   });
--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -3,28 +3,43 @@ import * as React from 'react';
 import { ReactElement } from 'react';
 
 export interface ErrorBoundaryProps {
-  fallbackUI: ReactElement<{}> | '';
+  fallbackUI: React.ReactElement<{}>;
 }
 
-export default class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error;
+}
+
+export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
   state = {
-    hasError: true
+    hasError: false,
+    error: {} as Error
   };
 
   componentDidCatch(error: Error, info: React.ErrorInfo) {
-    this.setState({ hasError: true });
+    this.setState({ 
+      hasError: true,
+      error
+    });
   }
 
   render() {
     if (this.state.hasError) {
-      return this.props.fallbackUI;
+      const Component = this.props.fallbackUI.type;
+      const props = {
+        ...this.props.fallbackUI.props,
+        error: this.state.error
+      };
+
+      return <Component {...props}/>;
     }
 
     return this.props.children;
   }
 }
 
-export const withErrorBoundary = (WrappedComponent, FallbackUI: React.ReactElement<{}> | '') =>
+export const withErrorBoundary = (WrappedComponent, FallbackUI: any) =>
   (props): ReactElement<{}> => (
     <ErrorBoundary fallbackUI={FallbackUI}>
       <WrappedComponent {...props} />
